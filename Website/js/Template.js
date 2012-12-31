86e0f25215a798e14ab5eaf00def698a7f7017a8
Max Rafferty <MaxPRafferty@gmail.com>
2012-12-31 10:14:44 -0800

    Initial Commit

---
+++ Website/js/Template.js
@@ -0,0 +1,18 @@
+﻿var template = function (requires, exports, modules) {
+    exports = exports || {};
+    var d = function (elementId) { return document.getElementById(elementId); }
+
+    var swapMenuState = function () {
+        var nav = d("MainNavigation");
+        if (nav.className.indexOf("Open") < 0) {
+            nav.className += " Open";
+        }
+        else {
+            nav.className = nav.className.replace(" Open", "");
+        }
+    }
+
+    exports.swapMenuState = swapMenuState;
+    return exports;
+}();
+
